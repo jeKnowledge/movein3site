2b838b2a7f358aa5e40ab077a07acfa80a8f429b
David Gomes <davidrafagomes@gmail.com>
2014-07-19 15:49:28 +0100

    Code style fixes.

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-
   /* ########## SLIDER ############## */
   $(function() {
     var newBg = ['wp-content/themes/movein3site/img/slider1.jpg',
                  'wp-content/themes/movein3site/img/slider2.jpg',
                  'wp-content/themes/movein3site/img/slider3.jpg'];
-
 
     var i = -1;
 
@@ -23,56 +21,32 @@ $(document).ready(function() {
 
 
   /* ######### BARRAS MENUS ########### */
-
-  $(window).scroll(function(){
-    if  ($(window).scrollTop() >= 478){
-      $('header').css({display:'block'});
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 478) {
+      $('header').css({ display:'block' });
     } else {
-      $('header').css({display:'none'});
+      $('header').css({ display:'none' });
     }
   });
-
-
-  /* ######## ANIMAÇÕES IMAGENS ############ */
-
-  /*$(window).bind("load resize scroll",function(e) {
-   var y = $(window).scrollTop();
-
-   $(".pageCover1").filter(function() {
-   return $(this).offset().top < (y + $(window).height()) &&
-   $(this).offset().top + $(this).height() > y;
-   }).css('background-position', '0px ' + parseInt(-y / 6) + 'px');
-
-   $(".pageCover2").filter(function() {
-   return $(this).offset().top < (y + $(window).height()) &&
-   $(this).offset().top + $(this).height() > y;
-   }).css('background-position', '0px ' + parseInt(-y / 6) + 'px');
-
-   $(".pageCover3").filter(function() {
-   return $(this).offset().top < (y + $(window).height()) &&
-   $(this).offset().top + $(this).height() > y;
-   }).css('background-position', '0px ' + parseInt(-y / 6) + 'px');
-   });*/
-
-
-
-
 
   /* ######## EFEITO SCROLL ######### */
 
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') &&
+          location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
             scrollTop: target.offset().top
           }, 2000);
+          
           return false;
         }
       }
+
+      return false;
     });
   });
-
 });
